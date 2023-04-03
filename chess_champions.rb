e9cc826a769f03396f8b7ceab49294d25a070f2c
Mathieu VALERY <66632538+mathieu-valery@users.noreply.github.com>
2023-04-03 16:56:15 +0200

    add faker for players

--- chess_champions.rb
+++ chess_champions.rb
@@ -1,41 +1,36 @@
-players = [
-    {
-      first_name: "Hikaru",
-      last_name: "Nakamura",
-      age: 35,
-      elo: 2400,
-    },
-    {
-      first_name: "Magnus",
-      last_name: "Carlsen",
-      age: 32,
-      elo: 2800,
-    },
-    {
-      first_name: "Andrea",
-      last_name: "Botez",
-      age: 20,
-      elo: 2000,
-    },
-    {
-      first_name: "Garry",
-      last_name: "Kasparov",
-      age: 59,
-      elo: 2230,
-    },
-]
+require 'faker'
+
+class Player
+  attr_reader :first_name, :last_name, :age, :elo
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @age = rand 18..75
+    @elo = rand 1000..3000
+  end
+end
+
+players = []
+
+for i in 0..1_000_000
+  players << Player.new(Faker::Name.first_name, Faker::Name.last_name)
+end
 
 def find_champions(players)
+  time_at_starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
   champions = []
   players.each do |player|
     next if champions.any? {|champion|
-      (champion[:elo] > player[:elo] && champion[:age] <= player[:age]) ||
-      (champion[:elo] >= player[:elo] && champion[:age] < player[:age])}
+      (champion.elo > player.elo && champion.age <= player.age) ||
+      (champion.elo >= player.elo && champion.age < player.age)}
     champions << player
     champions.delete_if do |champion|
-      (champion[:elo] < player[:elo] && champion[:age] >= player[:age]) ||
-      (champion[:elo] <= player[:elo] && champion[:age] > player[:age])
+      (champion.elo < player.elo && champion.age >= player.age) ||
+      (champion.elo <= player.elo && champion.age > player.age)
     end
   end
+  time_at_ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  time_elapsed = time_at_ending - time_at_starting
+  puts "find_champions: time time_elapsed #{time_elapsed} seconds"
   champions
 end
